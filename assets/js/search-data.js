16a6b7d5bfcf6744e78a64c6a85e6d854dd08367
joyce-lai <joycelai@umich.edu>
2026-02-10 21:20:08 +0000

    Deploying to gh-pages from @ joyce-lai/joyce-lai.github.io@df78567deab18aaa9bc2e59df841fd288455cff8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -444,7 +444,7 @@ ninja.data = [{
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
+              window.location.href = "/news/announcement_4/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
