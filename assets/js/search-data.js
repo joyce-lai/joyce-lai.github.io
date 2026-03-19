a2439a5988dd1187d3063b42518af804ba47c11e
joyce-lai <joycelai@umich.edu>
2026-03-19 20:11:19 +0000

    Deploying to gh-pages from @ joyce-lai/joyce-lai.github.io@5ac0eddf773b06968b630d62661c2c6bb5dd3588 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -482,7 +482,7 @@ ninja.data = [{
         title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.linkedin.com/in/jc-lai", "_blank");
+          window.open("https://www.linkedin.com/in/jclai-in", "_blank");
         },
       },{
         id: 'social-scholar',
